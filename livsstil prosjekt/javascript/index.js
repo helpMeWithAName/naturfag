53e705718222ca3a5a370a59268782a05410299f
helpMeWithAName <116715799+helpMeWithAName@users.noreply.github.com>
2022-12-02 23:57:05 +0100

    Add files via upload

---
+++ livsstil prosjekt/javascript/index.js
@@ -0,0 +1,17 @@
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() { 
+    document.getElementById("mySidebar").style.width = "15em";
+    document.getElementById("main").style.marginRight = "0";
+}
+  
+  /* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+  }
+
+
+
+
+ 
